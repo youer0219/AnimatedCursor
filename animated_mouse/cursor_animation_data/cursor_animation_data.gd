9785e9a7eb2cae06558521c9fa8d9d6a33671327
youer0219 <xlm20040219@qq.com>
2025-03-07 22:08:42 +0800

    为重构准备了数据类和一些方法

---
+++ animated_mouse/cursor_animation_data/cursor_animation_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name CursorAnimationData
+
+@export var name:StringName
+@export var cursor_offect:Vector2
+@export var scale:Vector2
--- animated_mouse/subview/subview_animation_cursor.gd
+++ animated_mouse/subview/subview_animation_cursor.gd
@@ -1,3 +1,4 @@
+@tool
 extends SubViewport
 class_name SubviewAnimationCursor
 
@@ -8,31 +9,26 @@ class_name SubviewAnimationCursor
 		if not is_node_ready():
 			await ready
 		
-		if not enable:
-			## TODO: 恢复默认？
-			cursor_animated_sprite2d.stop()
+		#if not enable:
+			### TODO: 恢复默认？
+			#cursor_animated_sprite2d.stop()
 
-@export var cursor_animated_sprite2d:AnimatedSprite2D:
-	set(value):
-		cursor_animated_sprite2d = value
-		
-		if not is_node_ready():
-			await ready
-		
-		if not cursor_animated_sprite2d.animation_changed.is_connected(_on_animated_sprite_2d_animation_changed):
-			cursor_animated_sprite2d.animation_changed.connect(_on_animated_sprite_2d_animation_changed)
-		if not cursor_animated_sprite2d.animation_finished.is_connected(_on_animated_sprite_2d_animation_finished):
-			cursor_animated_sprite2d.animation_changed.connect(_on_animated_sprite_2d_animation_finished)
-		_on_animated_sprite_2d_animation_changed()
-		_on_animated_sprite_2d_animation_finished()
+@export var sprite_frames:SpriteFrames = SpriteFrames.new()
+@export var cursor_animation_data_array:Array[CursorAnimationData]
+
+@onready var cursor_animated_sprite_2d: AnimatedSprite2D = $CursorAnimatedSprite2D
+
 
 func _ready() -> void:
-	if cursor_animated_sprite2d == null:
-		push_error("没有配置 cursor_animated_sprite2d 节点")
 	transparent_bg = true
 	disable_3d = true
 	render_target_update_mode = SubViewport.UPDATE_ALWAYS
-	
+
+	cursor_animated_sprite_2d.animation_changed.connect(_on_animated_sprite_2d_animation_changed)
+	cursor_animated_sprite_2d.animation_changed.connect(_on_animated_sprite_2d_animation_finished)
+	_on_animated_sprite_2d_animation_changed()
+	_on_animated_sprite_2d_animation_finished()
+
 	await get_tree().create_timer(2.0).timeout
 	play("runing")
 
@@ -46,16 +42,43 @@ func _process(_delta: float) -> void:
 	update_mouse_cursor_state()
 
 func play(anima_name:String):
-	if not cursor_animated_sprite2d.sprite_frames.get_animation_names().has(anima_name):
+	if not cursor_animated_sprite_2d.sprite_frames.get_animation_names().has(anima_name):
 		assert(false,"anima_name 不存在！请检查名称！")
 		return
-	cursor_animated_sprite2d.play(anima_name)
+	cursor_animated_sprite_2d.play(anima_name)
 
 func _get_curr_animtion_frame_texture_size()->Vector2:
-	return cursor_animated_sprite2d.sprite_frames.get_frame_texture(cursor_animated_sprite2d.animation,cursor_animated_sprite2d.frame).get_size() * cursor_animated_sprite2d.scale
+	return cursor_animated_sprite_2d.sprite_frames.get_frame_texture(cursor_animated_sprite_2d.animation,cursor_animated_sprite_2d.frame).get_size() * cursor_animated_sprite_2d.scale
 
 func _on_animated_sprite_2d_animation_changed() -> void:
 	size = _get_curr_animtion_frame_texture_size()
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	pass # Replace with function body.
+
+func _get_cursor_animation_data_names()->Array[StringName]:
+	var cursor_animation_data_names:Array[StringName] = []
+	for cursor_animation_data:CursorAnimationData in cursor_animation_data_array:
+		cursor_animation_data_names.append(cursor_animation_data.name)
+	return cursor_animation_data_names
+
+func _get_configuration_warnings():
+	var warnings = []
+	
+	## 检查 sprite_frames 配置 和 name 是否统一
+	if sprite_frames == null:
+		warnings.append("请生成一个 sprite_frames ")
+	else:
+		var anima_names = sprite_frames.get_animation_names()
+		if anima_names.size() == 0:
+			warnings.append("请在 sprite_frames 中配置动画效果")
+		else:
+			for corsor_anima_name:StringName in _get_cursor_animation_data_names():
+				if not anima_names.has(corsor_anima_name):
+					warnings.append("错误的name！请检查数据配置！")
+					break
+		
+	
+	## 可能的检查： 纹理大小 * 缩放系数 后是否满足条件
+	
+	return warnings
