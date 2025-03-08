b72dfa6f6e969ceaef453a4c24c4568d14bf907d
youer0219 <xlm20040219@qq.com>
2025-03-08 14:12:06 +0800

    暂时删除无用功能，等待重新设计实现

--- animated_mouse/animation_cursor.gd
+++ animated_mouse/animation_cursor.gd
@@ -1,19 +1,9 @@
 @tool
 extends SubViewport
 
-@export var enable:bool = true:
-	set(value):
-		enable = value
-		
-		if not is_node_ready():
-			await ready
-		
-		#if not enable:
-			### TODO: 恢复默认？
-			#cursor_animated_sprite2d.stop()
 
 @export var sprite_frames:SpriteFrames = SpriteFrames.new()
-
+@export var default_cursor_image:Texture2D
 @onready var cursor_animated_sprite_2d: AnimatedSprite2D = $CursorAnimatedSprite2D
 
 
@@ -22,17 +12,15 @@ func _ready() -> void:
 	disable_3d = true
 	render_target_update_mode = SubViewport.UPDATE_ALWAYS
 	
+	Input.set_custom_mouse_cursor(default_cursor_image,Input.CURSOR_ARROW,default_cursor_image.get_size()/2.0)
+	
 	cursor_animated_sprite_2d.sprite_frames = sprite_frames
 	cursor_animated_sprite_2d.animation_changed.connect(_on_animated_sprite_2d_animation_changed)
-	#cursor_animated_sprite_2d.animation_changed.connect(_on_animated_sprite_2d_animation_finished)
 	_on_animated_sprite_2d_animation_changed()
-	#_on_animated_sprite_2d_animation_finished()
 	
 	play("runing")
 
 func _process(_delta: float) -> void:
-	if not enable:
-		return
 	
 	if Engine.is_editor_hint():
 		if cursor_animated_sprite_2d.sprite_frames != sprite_frames:
@@ -41,10 +29,11 @@ func _process(_delta: float) -> void:
 	if sprite_frames == null:
 		return
 	
-	var new_texture = ImageTexture.create_from_image(get_texture().get_image())
-	## 强制为中心位置，暂无更好解决方法
-	Input.set_custom_mouse_cursor(new_texture,Input.CursorShape.CURSOR_ARROW , _get_curr_animtion_frame_texture_size() / 2.0)
-	update_mouse_cursor_state()
+	if cursor_animated_sprite_2d.is_playing():
+		var new_texture = ImageTexture.create_from_image(get_texture().get_image())
+		## 强制为中心位置，暂无更好解决方法
+		Input.set_custom_mouse_cursor(new_texture,Input.CursorShape.CURSOR_ARROW , _get_curr_animtion_frame_texture_size() / 2.0)
+		update_mouse_cursor_state()
 
 func play(anima_name:String):
 	if not sprite_frames.get_animation_names().has(anima_name):
@@ -58,12 +47,3 @@ func _get_curr_animtion_frame_texture_size()->Vector2:
 
 func _on_animated_sprite_2d_animation_changed() -> void:
 	size = _get_curr_animtion_frame_texture_size()
-
-func _get_configuration_warnings():
-	var warnings = []
-	
-	return warnings
-
-## 目前问题：希望有一个简单的易配置的动画节点完成动画工作
-## 与外界通信的方式
-## 我现在可以不需要连续执行动画的能力，但我需要配置不同动画的不同鼠标特性
